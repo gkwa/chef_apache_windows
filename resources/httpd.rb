10ebea79c2761de9927ee8af23bc7b748b03fa94
Taylor Monacelli <taylormonacelli@gmail.com>
2017-07-06 00:59:50 -0700

    Add template for custom resource

---
+++ resources/httpd.rb
@@ -0,0 +1,13 @@
+property :name, RubyType, default: 'value'
+
+load_current_value do
+  # some Ruby for loading the current state of the resource
+end
+
+action :name do
+ # a mix of built-in Chef resources and Ruby
+end
+
+action :name do
+ # a mix of built-in Chef resources and Ruby
+end
