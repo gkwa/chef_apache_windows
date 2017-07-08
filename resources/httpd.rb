432428a11718c1edbe4013072c58647423cb01d8
Taylor Monacelli <taylormonacelli@gmail.com>
2017-07-08 10:21:01 -0700

    Fix inspec test '[MAJR]  -v exit_status should eq 0'

--- resources/httpd.rb
+++ resources/httpd.rb
@@ -2,8 +2,8 @@ resource_name :httpd
 property :install_path, String, name_property: true
 property :instance_name, String
 property :version, String, default: '2.4.26'
-property :zip_x86_uri, String, default: 'https://www.apachelounge.com/download/VC15/binaries/httpd-2.4.26-Win32-VC15.zip'
-property :zip_x64_uri, String, default: 'https://www.apachelounge.com/download/VC15/binaries/httpd-2.4.26-Win64-VC15.zip'
+property :zip_x86_uri, String, default: 'https://www.apachelounge.com/download/VC14/binaries/httpd-2.4.26-Win32-VC14.zip'
+property :zip_x64_uri, String, default: 'https://www.apachelounge.com/download/VC14/binaries/httpd-2.4.26-Win64-VC14.zip'
 
 action :create do
   windows_zipfile new_resource.install_path do
