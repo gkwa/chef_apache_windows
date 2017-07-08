47cb72747059c5152d9d966e2f9f723dfde36f82
Taylor Monacelli <taylormonacelli@gmail.com>
2017-07-08 10:21:01 -0700

    Fix inspec test '[MAJR]  -v exit_status should eq 0'

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -1,7 +1,7 @@
-describe file('c:\Apache\bin\httpd.exe') do
+describe file('c:\Apache\Apache24\bin\httpd.exe') do
   it { should be_file }
 end
 
-describe command('c:\Apache\bin\httpd.exe -v') do
+describe command('c:\Apache\Apache24\bin\httpd.exe -v') do
   its('exit_status') { should eq 0 }
 end
