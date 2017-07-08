18a4fe93495386d375f2dc045ce41d093308effe
Taylor Monacelli <taylormonacelli@gmail.com>
2017-07-07 19:51:12 -0700

    Delete unit test since its not useful now

--- resources/httpd.rb
+++ resources/httpd.rb
@@ -3,18 +3,10 @@ property :zip_x86_uri, String, default: 'https://www.apachelounge.com/download/V
 property :zip_x64_uri, String, default: 'https://www.apachelounge.com/download/VC15/binaries/httpd-2.4.26-Win64-VC15.zip'
 property :install_path, String, default: 'C:\Program Files\Apache'
 
-load_current_value do
-
-end
-
 action :create do
   windows_zipfile install_path do
     source zip_x64_uri
     action :unzip
-    not_if {::File.exists?("#{install_path}/bin/httpd.exe")}
+    not_if { ::File.exist?("#{install_path}/bin/httpd.exe") }
   end
-end
-
-action :name do
-
 end
--- test/cookbooks/test/metadata.rb
+++ test/cookbooks/test/metadata.rb
@@ -7,4 +7,4 @@ long_description 'Installs/Configures test'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
-depends 'chef_dotfiles'
+depends 'chef_apache_windows'
