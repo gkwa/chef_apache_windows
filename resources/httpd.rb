e45000a03ea570bd63eb1686e3630b0bed8083b8
Taylor Monacelli <taylormonacelli@gmail.com>
2017-07-07 23:43:37 -0700

    Fix depreciation warning

--- resources/httpd.rb
+++ resources/httpd.rb
@@ -6,7 +6,7 @@ property :zip_x86_uri, String, default: 'https://www.apachelounge.com/download/V
 property :zip_x64_uri, String, default: 'https://www.apachelounge.com/download/VC15/binaries/httpd-2.4.26-Win64-VC15.zip'
 
 action :create do
-  windows_zipfile install_path do
+  windows_zipfile new_resource.install_path do
     source new_resource.zip_x64_uri
     action :unzip
     not_if { ::File.exist?("#{new_resource.install_path}/bin/httpd.exe") }
