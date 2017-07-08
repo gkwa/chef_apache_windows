98836de6ae4d28ef5547e29db67966957d95fb01
Taylor Monacelli <taylormonacelli@gmail.com>
2017-07-07 22:22:01 -0700

    Fix ArgumentError
    ArgumentError: You must supply a name when declaring a
    chef_apache_windows_httpd resource
    
    Full log
    https://ci.appveyor.com/project/TaylorMonacelli/chef-apache-windows/build/1.0.23#L202

--- resources/httpd.rb
+++ resources/httpd.rb
@@ -1,8 +1,8 @@
-property :instance_name, String, name_property: true
+property :install_path, String, default: 'C:\Program Files\Apache', name_property: true
+property :instance_name, String
 property :version, String, default: '2.4.26'
 property :zip_x86_uri, String, default: 'https://www.apachelounge.com/download/VC15/binaries/httpd-2.4.26-Win32-VC15.zip'
 property :zip_x64_uri, String, default: 'https://www.apachelounge.com/download/VC15/binaries/httpd-2.4.26-Win64-VC15.zip'
-property :install_path, String, default: 'C:\Program Files\Apache'
 
 action :create do
   windows_zipfile install_path do
--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -1,6 +1,6 @@
 include_recipe 'chef_apache_windows::default'
 
-chef_apache_windows_httpd do
+chef_apache_windows_httpd 'C:\Apache' do
   install_path 'C:\Apache'
   action :create
 end
