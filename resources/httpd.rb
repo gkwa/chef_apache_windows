afa477226caa693ee818e798f25b347f65ee32a8
Taylor Monacelli <taylormonacelli@gmail.com>
2017-07-08 12:37:56 -0700

    Use 32bit binary on 32bit machine, else allow choice

--- resources/httpd.rb
+++ resources/httpd.rb
@@ -2,12 +2,19 @@ resource_name :httpd
 property :install_path, String, name_property: true
 property :instance_name, String
 property :version, String, default: '2.4.26'
+property :use_32bit, [ true, false ], default: false
 property :zip_x86_uri, String, default: 'https://www.apachelounge.com/download/VC14/binaries/httpd-2.4.26-Win32-VC14.zip'
 property :zip_x64_uri, String, default: 'https://www.apachelounge.com/download/VC14/binaries/httpd-2.4.26-Win64-VC14.zip'
 
 action :create do
   windows_zipfile new_resource.install_path do
-    source new_resource.zip_x64_uri
+    zip_uri = new_resource.zip_x64_uri
+    if node['kernel']['machine'] != 'x86_64'
+      zip_uri = new_resource.zip_x86_uri
+    else
+      zip_uri = use_32bit ? new_resource.zip_x86_uri : new_resource.zip_x64_uri
+    end
+    source zip_uri
     action :unzip
     not_if { ::File.exist?("#{new_resource.install_path}/bin/httpd.exe") }
   end
